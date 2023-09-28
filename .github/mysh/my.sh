a96e86a28fc21446722bc9dab0ff4906e03f3b68
尘事尘飞 <86181542+fichenx@users.noreply.github.com>
2023-09-28 21:42:23 +0800

    Update my.sh

--- .github/mysh/my.sh
+++ .github/mysh/my.sh
@@ -9,13 +9,39 @@ git clone --depth 1 https://github.com/Leo-Jo-My/luci-theme-opentomcat
 git clone --depth 1 https://github.com/Leo-Jo-My/luci-theme-opentomato
 rm -rf luci-app-wechatpush
 git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush luci-app-serverchan
+
+
+#kenzok8/wall(将kenzok8自建常用的内核更改为breakings/OpenWrt/blob/main/diy-part2.sh中的源，只添加breakings中有的源)
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/chinadns-ng
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/dns2socks
+svn co https://github.com/fw876/helloworld/trunk/dns2tcp
 rm -rf brook
 svn export https://github.com/breakings/OpenWrt/trunk/general/brook
-#rm -rf hysteria
-#svn export https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria
-#rm -rf v2ray-core
-#svn export https://github.com/xiaorouji/openwrt-passwall-packages/trunk/v2ray-core
-
+rm -rf 
+svn export https://github.com/breakings/OpenWrt/trunk/general/dockerd
+svn export https://github.com/breakings/OpenWrt/trunk/general/gost
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/hysteria
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/ipt2socks
+svn co https://github.com/fw876/helloworld/trunk/lua-neturl
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/microsocks
+svn export https://github.com/breakings/OpenWrt/trunk/general/naiveproxy
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/pdnsd-alt
+svn co https://github.com/fw876/helloworld/trunk/redsocks2
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/shadowsocks-rust
+svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/simple-obfs
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/sing-box
+svn export https://github.com/breakings/OpenWrt/trunk/general/smartdns
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/ssocks
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/tcping
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/trojan-go
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/trojan-plus
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/trojan
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/v2ray-core
+svn co https://github.com/fw876/helloworld/trunk/v2ray-geodata
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/v2ray-plugin
+svn export https://github.com/breakings/OpenWrt/trunk/general/xray-core
+svn export https://github.com/breakings/OpenWrt/trunk/general/xray-plugin
 
 #svn export https://github.com/Boos4721/OpenWrt-Packages/trunk/adbyby
 #git clone --depth 1 https://github.com/aboutboy/luci-theme-butongwifi
