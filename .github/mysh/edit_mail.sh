6e3304e2f712a90e3a1f17daf73b1eae76f86271
尘事尘飞 <86181542+fichenx@users.noreply.github.com>
2023-09-28 20:25:42 +0800

    Update edit_mail.sh

--- .github/mysh/edit_mail.sh
+++ .github/mysh/edit_mail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #删除kenzok8/wall(kenzok8自建常用的内核)
-sed -i "s|git clone --depth 1 https://github.com/kenzok8/wall|#git clone --depth 1 https://github.com/kenzok8/wall|g" .github/diy/${{matrix.target}}.sh
+sed -i "s|git clone --depth 1 https://github.com/kenzok8/wall|#git clone --depth 1 https://github.com/kenzok8/wall|g" .github/diy/main.sh
