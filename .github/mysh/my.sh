7f7664171ab8a21d5262de5a6f26322cb74cad7e
尘事尘飞 <86181542+fichenx@users.noreply.github.com>
2024-01-09 18:36:40 +0800

    暂时替换原kenzok8/small-package/.github/diy/main.sh中无法使用的svn命令

--- .github/mysh/my.sh
+++ .github/mysh/my.sh
@@ -20,7 +20,7 @@ rm -rf $1
 ###########自定义部分##################
 git_sparse_clone master "https://github.com/Hyy2001X/AutoBuild-Packages" "Hyy2001X" luci-app-npc
 rm -rf luci-app-filebrowser filebrowser
-git_sparse_clone master "https://github.com/Lienol/openwrt-package" "Lienol" luci-app-filebrowser
+git_sparse_clone main "https://github.com/Lienol/openwrt-package" "Lienol" luci-app-filebrowser
 git clone --depth 1 https://github.com/Leo-Jo-My/luci-theme-opentomcat
 git clone --depth 1 https://github.com/Leo-Jo-My/luci-theme-opentomato
 rm -rf luci-app-wechatpush
@@ -28,16 +28,16 @@ git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush luci-ap
 
 
 #kenzok8/wall(将kenzok8自建常用的内核更改为breakings/OpenWrt/blob/main/diy-part2.sh中的源，只添加breakings中有的源)
-git_sparse_clone master "https://github.com/xiaorouji/openwrt-passwall-packages" "xiaorouji" chinadns-ng dns2socks hysteria ipt2socks \
+git_sparse_clone main "https://github.com/xiaorouji/openwrt-passwall-packages" "xiaorouji" chinadns-ng dns2socks hysteria ipt2socks \
 microsocks pdnsd-alt shadowsocks-rust simple-obfs sing-box ssocks tcping trojan-go trojan-plus trojan v2ray-core v2ray-plugin
 
-git_sparse_clone master "https:https://github.com/fw876/helloworld" "fw876" dns2tcp lua-neturl redsocks2 shadowsocksr-libev v2ray-geodata
+git_sparse_clone master "https://github.com/fw876/helloworld" "fw876" dns2tcp lua-neturl redsocks2 shadowsocksr-libev v2ray-geodata
 
 rm -rf brook dockerd gost naiveproxy smartdns xray-core xray-plugin
-git_sparse_clone master "https://github.com/breakings/OpenWrt" "breakings" general/brook general/dockerd general/gost general/naiveproxy \
+git_sparse_clone main "https://github.com/breakings/OpenWrt" "breakings" general/brook general/dockerd general/gost general/naiveproxy \
 general/smartdns general/xray-core general/xray-plugin
 
-#git_sparse_clone master "https://github.com/Boos4721/OpenWrt-Packages" "Boos4721" adbyby
+#git_sparse_clone packages-18.06 "https://github.com/Boos4721/OpenWrt-Packages" "Boos4721" adbyby
 #git clone --depth 1 https://github.com/aboutboy/luci-theme-butongwifi
 #git_sparse_clone master "https://github.com/Aslin-Ameng/luci-theme-Night" "Aslin-Ameng" luci-theme-Night
 #git clone --depth 1 https://github.com/gngpp/luci-theme-design
@@ -47,6 +47,25 @@ general/smartdns general/xray-core general/xray-plugin
 #git clone --depth 1 https://github.com/immortalwrt/homeproxy
 #git_sparse_clone master "https://github.com/coolsnowwolf/luci" "coolsnowwolf" applications/luci-app-accesscontrol
 git clone --depth 1 https://github.com/gngpp/luci-app-watchcat-plus
+
+
+############暂时替换原kenzok8/small-package/.github/diy/main.sh中无法使用的svn命令############
+git_sparse_clone master "https://github.com/immortalwrt/luci" "temp" applications/luci-app-homeproxy
+git_sparse_clone master "https://github.com/coolsnowwolf/luci" "temp" libs/luci-lib-ipkg
+git_sparse_clone master "https://github.com/x-wrt/packages" "temp" net/nft-qos
+git_sparse_clone master "https://github.com/x-wrt/luci" "temp" applications/luci-app-nft-qos
+git_sparse_clone other "https://github.com/Lienol/openwrt-package" "temp" lean/luci-app-autoreboot
+git_sparse_clone develop "https://github.com/Ysurac/openmptcprouter-feeds" "temp" luci-app-iperf
+git_sparse_clone master "https://github.com/QiuSimons/OpenWrt-Add" "temp" luci-app-irqbalance
+git_sparse_clone main "https://github.com/sirpdboy/sirpdboy-package" "temp" luci-app-control-speedlimit
+#git_sparse_clone master "https://github.com/xiaoxifu64/immortalwrt" "temp" package/rooter/ext-rooter-basic
+git_sparse_clone openwrt-22.03 "https://github.com/openwrt/luci" "temp" applications/luci-app-wireguard
+git_sparse_clone main "https://github.com/lucikap/Brukamen" "temp" luci-app-ua2f
+git_sparse_clone master "https://github.com/openwrt/packages" "temp" net/shadowsocks-libev
+git_sparse_clone main "https://github.com/kenzok8/jell" "temp" vsftpd-alt
+git_sparse_clone main "https://github.com/kenzok8/jell" "temp" luci-app-bridge
+############暂时替换原kenzok8/small-package/.github/diy/main.sh中无法使用的svn命令############
+
 
 # 修改nps源为yisier
 sed -i 's/PKG_SOURCE_URL:=.*/PKG_SOURCE_URL:=https:\/\/codeload.github.com\/yisier\/nps\/tar.gz\/v$(PKG_VERSION)?/g' nps/Makefile
